18892d887d328412e4e94bc0143d0a7c10d5f0a5
HazemUsama <hazemosama681@gmail.com>
2023-12-08 19:53:56 +0200

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,19 +3,15 @@
 
 content='
 <html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
+<head>
+</head>
+<body>
+Holberton School
+</body>
 </html>
 '
-new_server='
-location /hbnb_static {
-alias /data/web_static/current/;
-index index.html;
-}
-'
+new_server='\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}'
+
 sudo apt-get -y update
 sudo apt-get -y install nginx
 mkdir -p /data/web_static/shared/
@@ -24,5 +20,4 @@ touch /data/web_static/releases/test/index.html
 echo "$content" >  /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i "/server_name _;/a $new_server" /etc/nginx/sites-available/default
-
+sed -i "/server_name _;/a\ $new_server" /etc/nginx/sites-available/default
