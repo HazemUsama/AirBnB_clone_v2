de2ca13296917dbc58dfaaf110ba8717a82695ae
HazemUsama <hazemosama681@gmail.com>
2023-12-09 00:35:29 +0200

    hotfix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,13 +11,13 @@ def do_deploy(archive_path):
     """distributes an archive to my web servers"""
     if not os.path.exists(archive_path):
         return False
-
-    put('versions/{}'.format(archive_path), 'tmp/{}'.format(archive_path))
-    run('mkdir -p /data/web_static/releases/{}/'.format(archive_path))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_path, archive_path[:-4]))
-    run('rm /tmp/{}'.format(archive_path))
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_path[:-4], archive_path[:-4]))
+    archive_file = archive_path[9:]
+    put('archive_path', 'tmp/{}'.format(archive_file))
+    run('mkdir -p /data/web_static/releases/{}/'.format(archive_file))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_file, archive_file[:-4]))
+    run('rm /tmp/{}'.format(archive_file))
+    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_file[:-4], archive_file[:-4]))
     run('rm -rf /data/web_static/releases/web_static')
-    run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.format(archive_path[:-4]))
+    run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.format(archive_file[:-4]))
     print('New version deployed!')
     return True
