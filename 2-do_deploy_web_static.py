91ac007955f1aae34e531344eb606dbbe25cdb6d
HazemUsama <hazemosama681@gmail.com>
2023-12-09 00:40:40 +0200

    clean up

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,12 +12,14 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     archive_file = archive_path[9:]
+    release_version = '/data/web_static/releases/{}/'.format(archive_file[:-4])
     put('archive_path', 'tmp/{}'.format(archive_file))
-    run('mkdir -p /data/web_static/releases/{}/'.format(archive_file))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_file, archive_file[:-4]))
+    run('mkdir -p {}'.format(release_version))
+    run('tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
     run('rm /tmp/{}'.format(archive_file))
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_file[:-4], archive_file[:-4]))
+    run('mv {}/web_static/* {}'.format(release_version, release_version))
     run('rm -rf /data/web_static/releases/web_static')
-    run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.format(archive_file[:-4]))
+    run('ln -sf {} /data/web_static/current'.format(release_version))
+
     print('New version deployed!')
     return True
