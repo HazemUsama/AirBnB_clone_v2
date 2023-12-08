46f5087e07547626822c81bb5cf3b23c49c90e74
HazemUsama <hazemosama681@gmail.com>
2023-12-09 00:33:07 +0200

    fixed?

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,11 +6,12 @@ import os
 
 env.hosts = ['54.197.43.224', '52.201.178.140']
 
+
 def do_deploy(archive_path):
     """distributes an archive to my web servers"""
     if not os.path.exists(archive_path):
         return False
-    
+
     put('versions/{}'.format(archive_path), 'tmp/{}'.format(archive_path))
     run('mkdir -p /data/web_static/releases/{}/'.format(archive_path))
     run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_path, archive_path[:-4]))
@@ -18,3 +19,5 @@ def do_deploy(archive_path):
     run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_path[:-4], archive_path[:-4]))
     run('rm -rf /data/web_static/releases/web_static')
     run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.format(archive_path[:-4]))
+    print('New version deployed!')
+    return True
