173f04c9be32ed37bd47ce4637b65ef7b905c3de
HazemUsama <hazemosama681@gmail.com>
2023-12-08 23:05:36 +0200

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,8 +7,8 @@ import time
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static"""
     local("mkdir -p versions")
-    file_path = 'versions/web_static_{}.tgz'
-    .format(time.strftime('%Y%m%d%H%M%S'))
+    file_path = 'versions/web_static_{}.tgz'.format(
+            time.strftime('%Y%m%d%H%M%S'))
     results = local('tar -cvzf {} web_static'.format(file_path))
     if results.success:
         return file_path
