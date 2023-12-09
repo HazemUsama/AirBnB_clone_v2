b1227b68dedd046b98abe0300fe1e4335eebb4cf
HazemUsama <hazemosama681@gmail.com>
2023-12-09 02:03:49 +0200

    last fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,11 +7,12 @@ import os
 env.hosts = ['54.197.43.224', '52.201.178.140']
 env.user = 'ubuntu'
 
+
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static"""
     local("mkdir -p versions")
     archive_path = 'versions/web_static_{}.tgz'.format(
-    time.strftime('%Y%m%d%H%M%S'))
+        time.strftime('%Y%m%d%H%M%S'))
     results = local('tar -cvzf {} web_static'.format(archive_path))
     if results.success:
         return archive_path
@@ -24,15 +25,15 @@ def do_deploy(archive_path):
         return False
 
     archive_file = archive_path[9:]
-    release_version = '/data/web_static/releases/{}/'.format(archive_file[:-4])
+    release_version = '/data/web_static/releases/{}'.format(archive_file[:-4])
     put(archive_path, '/tmp/')
     run('mkdir -p {}'.format(release_version))
     run('tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
     run('rm /tmp/{}'.format(archive_file))
-    run('mv {}/web_static/* {}'.format(release_version, release_version))
-    run('rm -rf /data/web_static/releases/web_static')
-    run('rm -rf /data/web_static/current/')
-    run('ln -s {} /data/web_static/current'.format(release_version))
+    run('sudo mv {}/web_static/* {}'.format(release_version, release_version))
+    run('sudo rm -rf /data/web_static/releases/web_static')
+    run('sudo rm -rf /data/web_static/current')
+    run('sudo ln -s {} /data/web_static/current'.format(release_version))
 
     print('New version deployed!')
     return True
