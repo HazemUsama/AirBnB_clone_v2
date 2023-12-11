e52b0b237687d039b2e0bb9951f2564ff7cc640d
HazemUsama <hazemosama681@gmail.com>
2023-12-11 04:32:42 +0200

    outsource

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,41 +1,33 @@
-# Puppet to redo task 0
 
-# Install Nginx
-package { 'ngix':
-  ensure => installed,
+package { 'nginx':
+  ensure    => 'installed',
 }
 
-# Create Directory & Files
+file { '/data/web_static/shared/':
+  ensure    => 'directory',
+}
 
-file { [
-  '/data/web_static/',
-  '/data/web_static/releases/',
-  '/data/web_static/shared/',
-  '/data/web_static/releases/test/',
-]:
-  ensure  => directory,
+file { '/data/web_static/releases/test/':
+  ensure    => 'directory',
 }
 
 file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
+  ensure  => 'file',
   content => '<html><head></head><body>Holberton School</body></html>',
 }
 
-# Create a symbolic link
-file { '/data/web_static/current':
-  ensure => link,
+file { '/data/web_static/current/':
+  ensure => 'link',
   target => '/data/web_static/releases/test/',
   force  => true,
 }
 
 file { '/data/':
-  ensure  => directory,
+  recurse => true,
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  recurse => true,
 }
 
-# Restart Nginx
 service { 'nginx':
   ensure    => 'running',
   enable    => true,
