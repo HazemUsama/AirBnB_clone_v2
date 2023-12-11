1f138148927d5ad4347da3dba878cba5f51baa11
HazemUsama <hazemosama681@gmail.com>
2023-12-11 04:34:40 +0200

    again

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,36 +1,51 @@
-# Redo Task 0 with puppet
+# Redo task 0 with Puppet
 
+# Install Nginx if not already installed
 package { 'nginx':
-  ensure    => 'installed',
+  ensure => installed,
 }
 
-file { '/data/web_static/shared/':
-  ensure    => 'directory',
-}
-
-file { '/data/web_static/releases/test/':
-  ensure    => 'directory',
-}
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html><head></head><body>Holberton School</body></html>',
-}
-
-file { '/data/web_static/current/':
-  ensure => 'link',
-  target => '/data/web_static/releases/test/',
-  force  => true,
-}
-
+# Create directories
 file { '/data/':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
   recurse => true,
+}
+
+file { [
+  '/data/web_static/',
+  '/data/web_static/releases/',
+  '/data/web_static/shared/',
+  '/data/web_static/releases/test/',
+]:
+  ensure  => directory,
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
 
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+# Create a fake HTML file
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  content => '<html><head></head><body>Holberton School</body></html>',
 }
+
+# Create a symbolic link
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test/',
+  force  => true,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+# Service definition for Nginx
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+}
+
+
