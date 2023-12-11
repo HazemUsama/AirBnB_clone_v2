daecdbe3eb5f49d3b694bd43162f0894e1e994ea
HazemUsama <hazemosama681@gmail.com>
2023-12-11 04:13:52 +0200

    done

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,42 @@
+# Puppet to redo task 0
+
+# Install Nginx
+package { 'ngix':
+  ensure => installed,
+}
+
+# Create Directory & Files
+file { '/data/':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
+}
+
+file { [
+  '/data/web_static/',
+  '/data/web_static/releases/',
+  '/data/web_static/shared/',
+  '/data/web_static/releases/test/',
+]:
+  ensure  => directory,
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  content => '<html><head></head><body>Holberton School</body></html>',
+}
+
+# Create a symbolic link
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test/',
+  force  => true,
+}
+
+# Restart Nginx
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
