33a723cd01ad6e898da60d979adec419ffea0dc8
HazemUsama <hazemosama681@gmail.com>
2023-12-11 04:35:56 +0200

    another one

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,10 +7,9 @@ env.user = 'ubuntu'
 
 
 def do_clean(number=0):
-    """deletes out-of-date archives"""
-
-    path = '/data/web_static/releases'
+    """ deletes out-of-date archives """
     number = int(number)
+    path = '/data/web_static/releases'
 
     if number == 0:
         number = 2
@@ -19,4 +18,3 @@ def do_clean(number=0):
 
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
-   
