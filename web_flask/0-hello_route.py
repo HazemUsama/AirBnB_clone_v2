669d423313075e9816fa9d3a305f16eb75e54fbe
HazemUsama <hazemosama681@gmail.com>
2024-01-29 03:00:07 +0200

    fix pep

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 # Start a Flask web application
 
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_world():
-    return "<p>Hello, World!</p>"
+    """Return Hello HBNB"""
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run()
