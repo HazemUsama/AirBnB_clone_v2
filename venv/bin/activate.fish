bd5e336cdd8e95d43391dee35f061e5a367ae37c
HazemUsama <hazemosama681@gmail.com>
2024-01-29 02:55:18 +0200

    Hello Flask!

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/hazemosama/Work/AirBnB_clone_v2/venv"
+set -gx VIRTUAL_ENV "/home/hazem/Github/AirBnB_clone_v2/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
