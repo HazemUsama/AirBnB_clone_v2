e79cd5c56f026441d48848e6247e1066ca29ae01
HazemUsama <hazemosama681@gmail.com>
2024-01-29 03:14:08 +0200

    python is magic

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# Start a Flask web application
+"""
+starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Return Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    """Return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def C_is_fun(text):
+    """display “C ”, followed by the value of the text"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/<string:text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def python(text="is cool"):
+    """display Python, followed by the value of the text"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run()
