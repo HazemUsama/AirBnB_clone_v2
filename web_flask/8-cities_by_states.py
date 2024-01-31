1460568fe51dfb54d6e9393e52f66104b18eba68
HazemUsama <hazemosama681@gmail.com>
2024-01-31 03:12:23 +0200

    States and Cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,18 +5,19 @@ starts a Flask web application
 """
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+from models.state import State, City
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 @app.route("/states_list", strict_slashes=False)
-def show_states():
+def show_states_and_cities():
     """Display all states"""
     states = storage.all(State)
+    cities = storage.all(City)
 
-    return render_template('7-states_list.html', states=states.values())
+    return render_template('8-cities_by_states.html', states=states.values(), cities=cities.values())
 
 
 @app.teardown_appcontext
